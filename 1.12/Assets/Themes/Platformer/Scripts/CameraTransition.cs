08a5d52cdd56006b737322c635eb60e68918c9b9
Pete <38692750+PavelRasiukevich@users.noreply.github.com>
2021-03-06 16:33:36 +0300

    camera transition test

---
+++ 1.12/Assets/Themes/Platformer/Scripts/CameraTransition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+
+    private Vector2 bounds;
+    private BoxCollider2D[] colliders;
+
+    private void Awake()
+    {
+        colliders = new BoxCollider2D[2];
+
+        colliders = GetComponents<BoxCollider2D>();
+
+        foreach (var item in colliders)
+        {
+            if (item.isTrigger)
+            {
+                bounds = item.bounds.size;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        IKillable _obj = other.GetComponent<IKillable>();
+
+        if (_obj != null)
+            transform.position = new Vector3(transform.position.x + bounds.x, transform.position.y, transform.position.z);
+    }
+}
